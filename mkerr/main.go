11b154f8cf004f879efc335011c3b006b7af5b4e
skreimeyer <samuel.kreimeyer@gmail.com>
2019-07-07 22:00:23 -0500

    basic template structure added

--- mkerr/main.go
+++ mkerr/main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+func main() {
+	const tmpl := `
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug)]
+struct {{.name}} {
+    message: String
+}
+
+impl {{.name}} {
+    fn new(message: &str) -> {{.name}} {
+        {{.name}}{message: message.to_string()}
+    }
+}
+
+impl fmt::Display for {{.name}} {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"{}",self.message)
+    }
+}
+
+impl Error for {{.name}} {
+    fn description(&self) -> &str {
+        &self.message
+    }
+}`
+}
